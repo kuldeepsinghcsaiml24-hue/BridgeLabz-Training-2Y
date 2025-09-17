6d283df3e6515a729fb8c54269070431bce2986f
Kuldeep Singh <kuldeep.singh_cs.aiml24@gla.ac.in>
2025-09-17 12:29:04 +0530

    BMI & weight status using 2D array

---
+++ Array/Level2/BMICalculator2D.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BMICalculator2D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of persons: ");
+        int n = sc.nextInt();
+
+        double[][] data = new double[n][3];
+        String[] status = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Weight of person " + (i + 1) + " (kg): ");
+            data[i][0] = sc.nextDouble();
+            System.out.print("Height of person " + (i + 1) + " (m): ");
+            data[i][1] = sc.nextDouble();
+
+            data[i][2] = data[i][0] / (data[i][1] * data[i][1]);
+
+            if (data[i][2] < 18.5) status[i] = "Underweight";
+            else if (data[i][2] < 24.9) status[i] = "Normal";
+            else if (data[i][2] < 29.9) status[i] = "Overweight";
+            else status[i] = "Obese";
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Person " + (i + 1) +
+                " - H: " + data[i][1] + " W: " + data[i][0] +
+                " BMI: " + data[i][2] + " Status: " + status[i]);
+        }
+        sc.close();
+    }
+}
