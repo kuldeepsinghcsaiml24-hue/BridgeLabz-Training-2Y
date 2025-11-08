55584a7f10e7d0f0cb71a46bdbcb1e14f8561291
Kuldeep Singh <kuldeep.singh_cs.aiml24@gla.ac.in>
2025-11-08 20:22:39 +0530

    Added Extra String practice problem

---
+++ Strings/Extras/PalindromeCheck.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static boolean isPalindrome(String str) {
+        int n = str.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (str.charAt(i) != str.charAt(n - i - 1))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        if (isPalindrome(str))
+            System.out.println("Palindrome String");
+        else
+            System.out.println("Not a Palindrome");
+    }
+}
