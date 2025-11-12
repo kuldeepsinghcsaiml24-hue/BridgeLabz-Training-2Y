2af6f0f2c6c72aaccd70b5c31576b9da1e100cd9
Kuldeep Singh <kuldeep.singh_cs.aiml24@gla.ac.in>
2025-11-12 11:03:31 +0530

    Added solution of the extra Problem practice

---
+++ Core-Java/ControlFlow/Extras/FibonacciGen.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class FibonacciGen {
+    static void printFib(int n) {
+        int a = 0, b = 1;
+        System.out.print(a + " " + b + " ");
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+        printFib(n);
+    }
+}
