8bf45e949bafc130e100d8f8f136372bc1e62df4
kuldeepsinghcsaiml24-hue <kuldeep.singh_cs.aiml24@gla.ac.in>
2025-11-14 08:39:46 +0530

    Delete Collection directory

--- Collection/List/ElementFrequency.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class ElementFrequency {
-    public static void main(String[] args) {
-        List<String> list = Arrays.asList("apple", "banana", "apple", "orange");
-        Map<String, Integer> freq = new HashMap<>();
-        for (String s : list) freq.put(s, freq.getOrDefault(s, 0) + 1);
-        System.out.println(freq);
-    }
-}
